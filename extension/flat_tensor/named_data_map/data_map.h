c677c90eda4b42ae71876f00c521c355852ff282
lucylq <lfq@meta.com>
2025-01-24 11:26:38 -0800

    [executorch][flat_tensor] DataMap implementation
    Pull Request resolved: https://github.com/pytorch/executorch/pull/7900
    
    DataMap implementation that
    * Loads a flat_tensor file
    * Populates a map with {key: tensor} and {key: TensorLayout}.
    * Makes tensor information available via the named_data_map.h interface.
    
    For now, DataMap doesn't store the DataLoader.
    - If/when tensors are in their own segments, DataMap should also store a DataLoader.
    ghstack-source-id: 262957949
    
    Differential Revision: [D67064580](https://our.internmc.facebook.com/intern/diff/D67064580/)

---
+++ extension/flat_tensor/named_data_map/data_map.h
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <executorch/runtime/core/data_loader.h>
+#include <executorch/runtime/core/exec_aten/exec_aten.h>
+#include <executorch/runtime/core/named_data_map.h>
+#include <executorch/runtime/core/result.h>
+#include <executorch/runtime/core/tensor_layout.h>
+#include <executorch/runtime/platform/compiler.h>
+
+#include <unordered_map>
+#include <utility>
+
+// Forward declare flatbuffer types. This is a public header and must not
+// include the generated flatbuffer header.
+namespace flat_tensor_flatbuffer {
+struct FlatTensor;
+} // namespace flat_tensor
+
+namespace executorch {
+namespace extension {
+
+class DataMap final : public executorch::runtime::NamedDataMap {
+ public:
+  static executorch::runtime::Result<DataMap> load(
+      executorch::runtime::DataLoader* loader);
+
+  ET_NODISCARD
+  executorch::runtime::Result<const executorch::runtime::TensorLayout>
+  get_metadata(const char* key) const override;
+  ET_NODISCARD
+  executorch::runtime::Result<executorch::runtime::FreeableBuffer> get_data(
+      const char* key) const override;
+  ET_NODISCARD executorch::runtime::Result<size_t>
+  load_data_into(const char* key, void* buffer, size_t size) const override;
+
+  ET_NODISCARD executorch::runtime::Result<size_t> get_num_keys()
+      const override;
+  ET_NODISCARD executorch::runtime::Result<const char*> get_key(
+      size_t index) const override;
+
+  DataMap(DataMap&&) noexcept = default;
+  ~DataMap() override;
+
+ private:
+  DataMap(
+      executorch::runtime::FreeableBuffer&& flat_tensor_data,
+      std::unordered_map<
+          std::string,
+          std::tuple<int, int, executorch::runtime::TensorLayout>>
+          name_to_tensor,
+      executorch::runtime::FreeableBuffer&& data_ro)
+      : _flat_tensor_data(std::move(flat_tensor_data)),
+        _name_to_tensor(std::move(name_to_tensor)),
+        _data_ro(std::move(data_ro)) {}
+
+  // Not copyable or assignable.
+  DataMap(const DataMap& rhs) = delete;
+  DataMap& operator=(DataMap&& rhs) noexcept = delete;
+  DataMap& operator=(const DataMap& rhs) = delete;
+
+  // FlatTensor flatbuffer data. Contains the data backing up
+  // TensorLayout information in the _name_to_tensor map; must outlive it.
+  executorch::runtime::FreeableBuffer _flat_tensor_data;
+
+  // Map of name to {segment index, offset, TensorLayout}.
+  std::unordered_map<
+      std::string,
+      std::tuple<int, int, executorch::runtime::TensorLayout>>
+      _name_to_tensor;
+
+  // Raw, read-only tensor data.
+  executorch::runtime::FreeableBuffer _data_ro;
+};
+
+} // namespace extension
+} // namespace executorch
